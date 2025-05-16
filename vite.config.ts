7f1f94b24c8eabde50157cd4c380a9eea73d8ebd
Ahsas Sood <ahsassood@Ahsass-MacBook-Pro.local>
2025-05-16 10:55:41 +0530

    changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/ahsas-web-canvas/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
